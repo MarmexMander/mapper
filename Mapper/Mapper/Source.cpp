7cde5d20f137f79671395a21a1d2c12bfe289310
Marmex Mander <vadim.m.lutsk@gmail.com>
2019-07-25 17:18:15 +0300

    Переміщення по карті, спроба додати кнопку

--- Mapper/Mapper/Source.cpp
+++ Mapper/Mapper/Source.cpp
@@ -4,16 +4,38 @@
 #include <Windows.h>
 #include<fstream>
 #include<SFML/Graphics.hpp>
-#include <TGUI/TGUI.hpp>
-
 using namespace sf;
+Texture texture;
+class sButton {
+	Sprite s;
+public:
+	sButton(int TextureX, int TextureY, int x, int y) {
+		s.setTexture(texture);
+		s.setTextureRect(IntRect(TextureX * 32, TextureY * 32, 32, 32));
+		s.setPosition(x, y);
+	}
+	bool isPresed() {
+		Vector2i mouse = Mouse::getPosition();
+		Vector2f begin = s.getPosition(), end = s.getPosition() + s.getScale();
+		if (Mouse::isButtonPressed(Mouse::Left) && ((mouse.x >= begin.x) && (mouse.x <= end.x)) && ((mouse.y >= begin.y) && (mouse.y <= end.y))) {
+			s.setColor(Color::Black);
+			return 1;
+		}
+		else {
+			s.setColor(Color::White);
+			return 0;
+		}
+	}
+	Sprite getSprite() {
+		return s;
+	}
+};
 
 struct coords {
 	int x, y;
 };
 class tile {
 	Sprite s;
-	Texture texture;
 	coords textureRectPos;
 	coords altTextureRectPos;
 	short int type = 1;
@@ -33,9 +55,9 @@ class tile {
 public:
 	tile() {
 		textureRectPos.x = 0, textureRectPos.y = 0;
-		texture.loadFromFile("Textures/tileset.png");
-		s.setTexture(texture);
-		s.setTextureRect(IntRect(textureRectPos.x, textureRectPos.y, 32, 32));
+		
+			s.setTexture(texture);
+			s.setTextureRect(IntRect(textureRectPos.x, textureRectPos.y, 32, 32));
 	}
 	void use() {
 		coords buffTextureRectPos;
@@ -58,7 +80,7 @@ public:
 		}
 	}
 	Sprite setPos(int x, int y) {
-		s.setPosition(x*32, y*32);
+		s.setPosition(x * 32, y * 32);
 		return s;
 	}
 	void setTexture(int x, int y) {
@@ -68,15 +90,17 @@ public:
 };
 tile **tileMap;
 int main() {
-	int tileMap_Y = 20, tileMap_X = 20;
-	sf::RenderWindow window{ {800, 600}, "Window" };
+	if (!texture.loadFromFile("textures.png"))
+		exit(-12);
+	int tileMap_Y = 30, tileMap_X = 22, cameraOffset_X = 0, cameraOffset_Y = 0;
+	sButton lButton(17, 22, 23*32, 1);
+	sf::RenderWindow window{ {1280, 720}, "Window" };
 	tileMap = new tile*[tileMap_Y];
 	RectangleShape tileRect;
 	for (size_t i = 0; i < tileMap_Y; i++)
 	{
 		tileMap[i] = new tile[tileMap_X];
 	}
-
 	while (window.isOpen())
 	{
 		Event event;
@@ -84,22 +108,50 @@ int main() {
 		{
 			if (event.type == sf::Event::Closed)
 				window.close();
+			if (event.type == Event::KeyPressed) {
+				switch (event.key.code) {
+				case Keyboard::Up:
+					if (cameraOffset_X != 0)
+						cameraOffset_X--;
+					break;
+				case Keyboard::Down:
+					if (cameraOffset_X != tileMap_X - ((tileMap_X < 20) ? tileMap_X : 20))
+						cameraOffset_X++;
+					break;
+				case Keyboard::Left:
+					if (cameraOffset_Y != 0)
+						cameraOffset_Y--;
+					break;
+				case Keyboard::Right:
+					if (cameraOffset_Y != tileMap_Y - ((tileMap_Y < 20) ? tileMap_Y : 20))
+						cameraOffset_Y++;
+					break;
+				}
+
+			}
+			if (lButton.isPresed()) {
+
+			}
 		}
 
 		if (Mouse::isButtonPressed(Mouse::Left)) {
 			Vector2i mousePos = Mouse::getPosition(window);
 			mousePos.x /= 32;
 			mousePos.y /= 32;
-			tileMap[mousePos.x][mousePos.y].setTexture(32,32);
+			mousePos.x += cameraOffset_X;
+			mousePos.y += cameraOffset_Y;
+			if ((mousePos.x < tileMap_X && mousePos.x > -1) && (mousePos.y < tileMap_Y && mousePos.y > -1))
+				tileMap[mousePos.x][mousePos.y].setTexture(32, 32);
 		}
 
-		for (size_t j = 0; j < tileMap_Y; j++)
+		for (size_t j = cameraOffset_Y, jj = 0; j < ((tileMap_Y < 20) ? tileMap_Y : 20)+cameraOffset_Y; j++, jj++)
 		{
-			for (size_t i = 0; i < tileMap_X; i++)
+			for (size_t i = cameraOffset_X, ii = 0; i < ((tileMap_X < 20) ? tileMap_X : 20)+cameraOffset_X; i++, ii++)
 			{
-				window.draw(tileMap[i][j].setPos(i,j));
+				window.draw(tileMap[j][i].setPos(jj, ii));
 			}
 		}
+		window.draw(lButton.getSprite());
 		window.display();
 		window.clear();
 	}
